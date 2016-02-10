112b830c4f831312aeb6ea5749c09f18b3857913
Terence Nip <terence.nip@gmail.com>
2016-02-10 15:22:52 -0600

    distribute mp2

---
+++ assignments/mp2-interpreter/Interpreter.hs
@@ -0,0 +1,60 @@
+{-----------------------------------
+ - Interpreter.hs
+ - v1.0
+ -----------------------------------}
+
+module Interpreter where
+
+-- Language Representation
+import Data.HashMap.Strict as H
+import Text.ParserCombinators.Parsec
+import Parser
+
+fixMe = error "fix me!"
+
+liftIntOp op (IntVal x) (IntVal y) = IntVal $ op x y
+liftIntOp _ _ _ = ExnVal "Cannot lift"
+
+liftBoolOp op (BoolVal x) (BoolVal y) = BoolVal $ op x y
+liftBool _ _ _ = ExnVal "Cannot lift"
+
+liftCompOp :: (Int -> Int -> Bool) -> Val -> Val -> Val
+liftCompOp = fixMe
+liftCompOp _ _ _ = ExnVal "Cannot lift"
+
+{-----------------------------------
+ - eval: The Evaluator
+ -----------------------------------}
+eval :: Exp -> Env -> Val
+eval _ _ = fixMe
+
+
+{-----------------------------------
+ - exec
+ -----------------------------------}
+exec :: Stmt -> PEnv -> Env -> Result
+exec (PrintStmt e) penv env = (val, penv, env)
+   where val = show $ eval e env
+exec _ _ _ = fixMe
+
+{-----------------------------------
+ - repl
+ -----------------------------------}
+repl :: PEnv -> Env -> [String] -> String -> IO Result
+repl penv env [] _ =
+  do putStr "> "
+     input <- getLine
+     case parse stmt "stdin" input of
+        Right QuitStmt -> do putStrLn "Bye!"
+                             return ("",penv,env)
+        Right x -> let (nuresult,nupenv,nuenv) = exec x penv env
+                   in do {
+                     putStrLn nuresult;
+                     repl nupenv nuenv [] "stdin"
+                   }
+        Left x -> do putStrLn $ show x
+                     repl penv env [] "stdin"
+
+main = do
+  putStrLn "Welcome to your interpreter!"
+  repl H.empty H.empty [] "stdin"
