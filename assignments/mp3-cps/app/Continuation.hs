fa0208044f76d190681656a46c30a9c9e2e48f31
Shan Zhao <szhao28@illinois.edu>
2016-03-06 22:09:59 -0600

    working

--- assignments/mp3-cps/app/Continuation.hs
+++ assignments/mp3-cps/app/Continuation.hs
@@ -21,29 +21,69 @@ repl () =
  - Problem 1: factk 
  --------------------------------------}
 factk :: Integer -> (Integer -> a) -> a
-factk = fixMe
+
+factk 0 k = k 1
+factk n k = factk (n-1) (\ v -> k (n * v))
 
 {--------------------------------------
  - Problem 2: evenoddk
  --------------------------------------}
 evenoddk :: Integral r => [r] -> (r -> t) -> (r -> t) -> t
-evenoddk = fixMe
+evenoddk [x] k1 k2
+         | even x = k1 x
+         | odd x = k2 x
+evenoddk (x:xs) k1 k2
+            | even x = evenoddk xs (\v -> k1 (v + x)) k2
+            | odd x = evenoddk xs k1 (\v -> k2 (v + x))
+
 
 {--------------------------------------
  - Problem 3: isSimple
  --------------------------------------}
 isSimple :: Exp -> Bool
-isSimple = fixMe
+
+isSimple (IntExp a) = True
+
+isSimple (VarExp s) = True
+
+isSimple (AppExp e1 e2) = False
+
+isSimple (OpExp s e1 e2) = isSimple e1 && (isSimple e2)
+
+isSimple (IfExp e1 e2 e3) = isSimple e1 && (isSimple e2) && (isSimple e3)
+
 
 {--------------------------------------
  - Problem 4: cpsDecl, cpsExp
  --------------------------------------}
 cpsDecl :: Stmt -> Stmt
-cpsDecl = fixMe
+
+cpsDecl (Decl s1 xx e) = Decl s1 (xx++["k"]) e1
+          where (e1,_) = cpsExp e (VarExp "k") 0
 
 cpsExp :: Exp -> Exp -> Integer -> (Exp,Integer)
-cpsExp = fixMe
+cpsExp (IntExp i) k j = (AppExp k (IntExp i), j)
+cpsExp (VarExp v) k j = (AppExp k (VarExp v), j)
+cpsExp (AppExp e1 e2) k j
+              | isSimple e2 = (AppExp (AppExp e1 e2) k, j)
+              | otherwise = cpsExp e2 (LamExp m (AppExp (AppExp e1 (VarExp m)) k)) (j + 1)
+                  where (m,_) = gensym j
 
+cpsExp (IfExp e1 e2 e3) k j
+                 | isSimple e1 = (IfExp e1 e4 e5, j)
+                 | otherwise = cpsExp e1 (LamExp m (IfExp (VarExp m) e4 e5)) (j + 1)
+                     where (e4,_) = cpsExp e2 k j
+                           (e5,_) = cpsExp e3 k j
+                           (m,_) = gensym j
+
+cpsExp (OpExp s e1 e2) k j
+              | isSimple e1 && (isSimple e2) = (AppExp k (OpExp s e1 e2), j)         
+              | not (isSimple e1) && (isSimple e2) = cpsExp e1 (LamExp v (AppExp k (OpExp s (VarExp v) e2))) (j + 1)
+              | isSimple e1 && (not (isSimple e2)) = cpsExp e2 (LamExp v (AppExp k (OpExp s e1 (VarExp v)))) (j + 1)
+              | otherwise = cpsExp e1 (LamExp v e3) l
+                 where (v,_) = gensym j
+                       (e3,l) = cpsExp e2 (LamExp v1 (AppExp k (OpExp s (VarExp v) (VarExp v1)))) (j + 2)
+                       (v1,_) = gensym (j + 1)
 {--------------------------------------
  - Helper Functions
  --------------------------------------}
