c1855ab67c69e9e2a10091f6acb29ef07e3719a7
Everett Hildenbrandt <hildenb2@illinois.edu>
2016-04-10 23:22:27 -0500

    handout to version 1.4.0, tests to version 1.4.1

--- assignments/mp4-scheme/test/Tests.hs
+++ assignments/mp4-scheme/test/Tests.hs
@@ -56,6 +56,9 @@ primitiveMiscOrig = (   [ ["()"]
                         , [ "(+)", "(-)", "(*)", "(>)", "(<)", "(>=)", "(<=)", "(=)", "(!=)"
                           , "(eq?)", "(and)", "(or)", "(not)", "(list)", "(car)", "(cdr)"
                           ]
+                        , [ "(+ 1)", "(- 1)", "(* 1)", "(> 1)", "(< 1)", "(>= 1)", "(<= 1)", "(= 1)", "(!= 1)"
+                          , "(eq? 'a)", "(eq? 1)", "(and 't)", "(or 'nil)", "(not 't)" , "(list 't)"
+                          ]
                         , [ "(eq? 5 5)", "(eq? 6 5 6)", "(eq? (+ 3 3) 6)"
                           , "(eq? 'a 5)", "(eq? 'a 'b)", "(eq? 'a 'a 'b)", "(eq? 'a 'a 'a)"
                           ]
@@ -64,6 +67,7 @@ primitiveMiscOrig = (   [ ["()"]
                     ,   [ "Do you handle the empty form `()` correctly?"
                         , "Do you handle the base-cases of the primitive operators correctly?"
                         , "Do you handle `eq?` correctly?"
+                        , "Do you handle singleton cases properly for primitives (single argument)?"
                         ]
                     )
 
@@ -78,10 +82,12 @@ consListCarCdrOrig =    (   [ ["cons", "list", "car", "cdr"]
                             , [ "(car (cons 2 (cons 3 (cons 4 'nil))))", "(cdr (cons 2 (cons 3 (cons 4 'nil))))"
                               , "(car (list 2 3 4 'a 'b))", "(cdr (list 'b 'c 3 4 (+ 3 4)))"
                               ]
+                            , [ "(car (cons 1 2) (cons 2 3))", "(cdr (cons 2 't) (list 4 2 4 5))" ]
                             ]
 
                         ,   [ "Are you handling primitives `list`, `car`, and `cdr` correctly?"
                             , "Are you handling the `cons` form correctly?"
+                            , "Do you produce an `ExnVal` if `car` or `cdr` is applied to too many arguments?"
                             ]
                         )
 
@@ -91,11 +97,31 @@ defDefineLambdaApplyOrig =  (   [ ["(def x (+ 10 20))", "x", "y"]
                                 , [ "(lambda (x) (+ x 10))", "( (lambda (x) (+ x 10)) 20)"
                                   , "(define mkInc (x) (lambda (y) (+ x y)))", "(def i2 (mkInc 2))", "(i2 10)"
                                   ]
+                                , [ "(define f ((+ 3 4) x y) (+ x y))", "(def (+ 3 4) 5)" ]
+                                , [ "(define f (x y z) (+ (- x y) z))", "(f 3 4 2 8 0)" ]
                                 ]
 
                             ,   [ "Do you handle constant definition correctly?"
                                 , "Do you handle function definition/closure application correctly?"
                                 , "Do you handle printing a `Closure` as `*closure*`?"
+                                , "Do you handle `def` and `define` having non-symbol parameter names?"
+                                , "For a `Closure` applied to extra arguments, are the extra ones thrown away?"
+                                ]
+                            )
+
+allowsDefOverPrimFormOrig :: ([[String]], [String])
+allowsDefOverPrimFormOrig = (   [ [ "(def define 3)", "define" ]
+                                , [ "(define let (x y z) (+ x y z))", "(let 3 4 5)"
+                                  , "(let ((x 5) (y 100) (z 3)) (+ x (- y z)))"
+                                  ]
+                                , [ "(def + 3)" , "(- + 2)", "(* + 10)" ]
+                                , [ "(define quote (x y) (list x y))", "(quote 'a 'b)", "(quote 'a)" ]
+                                ]
+
+                            ,   [ "Do you allow users to `define` or `def` variables with the same name"
+                                , "as primitive operators? If you redefine a form (such as `let` or `quote`)"
+                                , "as an operator, but it's used correctly as a form, the definition as a"
+                                , "form should be used. Otherwise the environment lookup should be used."
                                 ]
                             )
 
@@ -150,6 +176,9 @@ nestedQuasiQuotesOrig = (   [ [ "(def a 5)", "``(+ ,,a 1)", "``(+ ,,a ,a)", "`(+
                               , "(eval ``(+ ,,a 1))", "(eval (eval ``(+ ,,a 1)))"
                               ]
                             , [ "(def a 5)", "```(+ ,,,a ,,a)", "```(+ ,a ,,a)", "```(+ `a `(+ ,,,,a a))" ]
+                            , [ "(def a 5)", "```a", "```,a", "```,,a", "```,,,a"
+                              , "(eval ```,,,a)", "(eval (eval ```,,,a))"
+                              ]
                             ]
 
                         ,   [ "Do you handle nested quasiquotes?" ]
