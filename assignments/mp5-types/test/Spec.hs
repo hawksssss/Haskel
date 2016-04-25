05e9dcacc73d7046515875d7b843a14dcb171295
Terence Nip <terence.nip@gmail.com>
2016-04-25 10:54:36 -0500

    update mp5 tests

--- assignments/mp5-types/test/Spec.hs
+++ assignments/mp5-types/test/Spec.hs
@@ -126,6 +126,7 @@ prop_unifyRedux n = do {
                        ; rightTests <- sample' $ listOf1 (tycon' 1)
                        ; let intList = map TyVar [1..numVars]
                        ; let tests = zip intList (concat rightTests)
+--                       ; putStrLn ("Testing: " ++ (show tests)) -- Uncomment this line to see the test you're stuck on.
                        ; let actual = transformUnify $ U.unify tests
                        ; let test = sillyUnify tests []
                        ; result <- if (actual == test) then do {return True} else do { putStrLn "Failed\n\n"; print $ show tests; print actual; print test; return False}
